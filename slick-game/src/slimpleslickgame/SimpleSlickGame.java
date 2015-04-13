5cf3b67f09b62038b275596948d3e1567c1bcca4
Dolck <tim.dolck@gmail.com>
2015-04-13 16:40:18 +0200

    better movements with key polling

--- slick-game/src/slimpleslickgame/SimpleSlickGame.java
+++ slick-game/src/slimpleslickgame/SimpleSlickGame.java
@@ -1,133 +1,90 @@
 package slimpleslickgame;
+
 import java.awt.Dimension;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.SpriteSheet;
-import org.newdawn.slick.command.BasicCommand;
-import org.newdawn.slick.command.Command;
-import org.newdawn.slick.command.ControllerDirectionControl;
-import org.newdawn.slick.command.InputProvider;
-import org.newdawn.slick.command.InputProviderListener;
-import org.newdawn.slick.command.KeyControl;
 import org.newdawn.slick.geom.Vector2f;
 
-public class SimpleSlickGame extends BasicGame implements InputProviderListener {
+public class SimpleSlickGame extends BasicGame {
 
-	private Image[] shipDefault, shipLeft, shipRight;
-	private Animation sprite, spaceShipDefault, spaceShipLeft, spaceShipRight;
-	private SpriteSheet spaceShipSpriteSheet;
-	private InputProvider provider;
+	// TODO: sprites can wait, rectangles are fine
+	// private Image[] shipDefault, shipLeft, shipRight;
+	// private Animation sprite, spaceShipDefault, spaceShipLeft,
+	// spaceShipRight;
+	// private SpriteSheet spaceShipSpriteSheet;
+
 	private Player player;
-	
-	private final String moveLeftS = "moveLeft";
-	private final String moveRightS = "moveRight";
-	private final String moveUpS = "moveUp";
-	private final String moveDownS = "moveDown";
-	private final String attackS = "attack";
-	
-	private Command moveLeft = new BasicCommand(moveLeftS);
-	private Command moveRight = new BasicCommand(moveRightS);
-	private Command moveUp = new BasicCommand(moveUpS);
-	private Command moveDown = new BasicCommand(moveDownS);
-	private Command attack = new BasicCommand(attackS);
-	
+
 	public SimpleSlickGame(String gamename) {
 		super(gamename);
-		//TODO: Connect with server
-		
+		// TODO: Connect with server
+
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		provider = new InputProvider(gc.getInput());
-		provider.addListener(this);
-		
-		provider.bindCommand(new KeyControl(Input.KEY_LEFT), moveLeft);
-		provider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.LEFT), moveLeft);
-		provider.bindCommand(new KeyControl(Input.KEY_RIGHT), moveRight);
-		provider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.RIGHT), moveRight);
-		provider.bindCommand(new KeyControl(Input.KEY_UP), moveUp);
-		provider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.UP), moveUp);
-		provider.bindCommand(new KeyControl(Input.KEY_DOWN), moveDown);
-		provider.bindCommand(new ControllerDirectionControl(0, ControllerDirectionControl.DOWN), moveDown);
-		provider.bindCommand(new KeyControl(Input.KEY_SPACE), attack);
-		
+		// TODO: Setup game stuff
+
 		player = new Player();
 		player.init();
-		//TODO: Setup game stuff
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
-		//TODO: Update all objects
-		player.update();
+		// TODO: Update all objects
+		processInput(gc.getInput());
+		player.update(delta);
 	}
 
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
-		g.drawString("Howdy!", 100, 100);
-		g.drawRect(50, 50, 200, 400);
-		g.drawRect(250, 50, 200, 400);
+	private void processInput(Input input) {
+		Vector2f direction = new Vector2f(0, 0);
+
+		if (input.isKeyDown(Input.KEY_LEFT)) {
+			direction.add(new Vector2f(-1, 0));
+		}
+
+		if (input.isKeyDown(Input.KEY_RIGHT)) {
+			direction.add(new Vector2f(1, 0));
+		}
+
+		if (input.isKeyDown(Input.KEY_UP)) {
+			direction.add(new Vector2f(0, -1));
+		}
+
+		if (input.isKeyDown(Input.KEY_DOWN)) {
+			direction.add(new Vector2f(0, 1));
+		}
+
+		// TODO: add shooting capabilities
+
+		player.setDirection(direction);
+	}
+
+	public void render(GameContainer gc, Graphics g) throws SlickException {
 		player.render(g);
 	}
 
-	public static void main(String[] args)
-	{
-		try
-		{
+	public static void main(String[] args) {
+		try {
 			AppGameContainer appgc;
-			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+			appgc = new AppGameContainer(new SimpleSlickGame(
+					"Simple Slick Game"));
 			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 			appgc.setDisplayMode(
 					new Double(screenSize.width * 0.75).intValue(), new Double(
 							screenSize.getHeight() * 0.75).intValue(), false);
 			appgc.start();
+		} catch (SlickException ex) {
+			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE,
+					null, ex);
 		}
-		catch (SlickException ex)
-		{
-			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
-
-	@Override
-	public void controlPressed(Command cmd) {
-		// TODO Auto-generated method stub
-		BasicCommand bcmd = (BasicCommand) cmd;
-		System.out.println(bcmd.toString());
-		float x, y;
-		switch(bcmd.getName()){
-		case moveLeftS:
-			x = -1;
-			y = 0;
-			break;
-		case moveRightS:
-			x = 1;
-			y = 0;
-			break;
-		default:
-			x = 0;
-			y = 0;
-			break;
-		}
-		
-		player.changeDirection(x, y);
-		
-		
-	}
-
-	@Override
-	public void controlReleased(Command cmd) {
-		// TODO Auto-generated method stub
-		
 	}
 }
