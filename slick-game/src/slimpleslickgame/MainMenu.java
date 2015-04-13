1d462f59d39541ea9c805b9331435dc8b2ff6ffa
Jullekungfu <julian.krone@hotmail.com>
2015-04-13 11:33:19 +0200

    splashscreen done, init main menu

--- slick-game/src/slimpleslickgame/MainMenu.java
+++ slick-game/src/slimpleslickgame/MainMenu.java
@@ -7,31 +7,25 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class MainMenu extends BasicGameState {
+    private static final int WIDTH   = 1920;
+    private static final int HEIGHT  = 1080;
 	public static final int ID = 1;
 	private StateBasedGame game;
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		this.game = game;
 
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
-			throws SlickException {
-		// TODO Auto-generated method stub
-		
-
-		g.drawString("NEW GAME", 200, 200);
-		g.drawString("JOIN GAME", 200, 300);
-
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		g.drawString("NEW GAME", WIDTH/2 - 50, 200);
+		g.drawString("JOIN GAME", WIDTH/2 - 50, 300);
 	}
 
 	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		// TODO Auto-generated method stub
 
 	}
--- slick-game/src/slimpleslickgame/SplashScreen.java
+++ slick-game/src/slimpleslickgame/SplashScreen.java
@@ -1,12 +1,12 @@
 package slimpleslickgame;
 
+import java.awt.Font;
+
 import org.newdawn.slick.Color;
-import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.TrueTypeFont;
-import org.newdawn.slick.font.HieroSettings;
+import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
@@ -20,30 +20,32 @@ public class SplashScreen extends BasicGameState {
 	private StateBasedGame game;
 	private int time;
 	private Font font;
+	private UnicodeFont ufont;
+	private String text;
+	private int tw, th;
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game)
-			throws SlickException {
-		// TODO Auto-generated method stub
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		this.game = game;
 		time = 0;
+
 				
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g)
-			throws SlickException {
-		g.drawString("SPLASHSCREEN", WIDTH/2, HEIGHT/2);
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		int th = g.getFont().getLineHeight()/2;
+		int tw = 50;
+		
+		g.drawString("SPLASHSCREEN", (WIDTH/2)-tw, (HEIGHT/2)-th);
 	}
 
 	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta)
-			throws SlickException {
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
 		time += delta;
 		if(time > 3000){
 			game.enterState(MainMenu.ID, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
 		}
-
 	}
 
 	@Override
