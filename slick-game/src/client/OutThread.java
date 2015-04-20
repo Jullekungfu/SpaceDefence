0df6f306af2ee9a97a4ff26b48838066612e97f8
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-04-20 11:37:32 +0200

    Added comments to outthread.

--- slick-game/src/client/OutThread.java
+++ slick-game/src/client/OutThread.java
@@ -4,11 +4,20 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
+/**
+ * @author nille
+ *
+ */
 public class OutThread extends Thread {
 	private OutputStream os;
 	private Socket socket;
 	private ByteMonitor bm;
 	
+	/**
+	 * Constructor for OutThread. 
+	 * @param bm The ByteMonitor connected to this OutThread
+	 * @param s The socket in which the thread writes to.
+	 */
 	public OutThread(ByteMonitor bm, Socket s){
 		this.socket = s;
 		this.bm = bm;
