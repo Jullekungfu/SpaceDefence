d6b267b838ef4c7fdb0db68681e0d31de3b0fabf
Dolck <tim.dolck@gmail.com>
2015-04-20 11:37:52 +0200

    parsing bytes

--- slick-game/src/client/ParseBytes.java
+++ slick-game/src/client/ParseBytes.java
@@ -1,5 +1,7 @@
 package client;
 
+import org.newdawn.slick.geom.Vector2f;
+
 
 /**
  * Thread to parse bytes to gamestats
@@ -19,7 +21,34 @@ public class ParseBytes extends Thread {
 	
 	@Override
 	public void run(){
-		//Parse bytes to gamestats
+		while(true){
+			byte[] bytes = bMonitor.readArrayFromServer();
+			int parsed = 0;
+			byte id = bytes[parsed++];
+			byte hp = bytes[parsed++];
+			
+			byte[] floatBytes = new byte[4];
+			for(int i = 0; i < 4; i++){
+				floatBytes[i] = bytes[parsed++];
+			}
+			float xpos = bytesToFloat(floatBytes);
+			for(int i = 0; i < 4; i++){
+				floatBytes[i] = bytes[parsed++];
+			}
+			float ypos = bytesToFloat(floatBytes);
+			Vector2f pos = new Vector2f(xpos, ypos);
+			
+			//Parse bytes to gamestats	
+		}
+	}
+	
+	private float bytesToFloat(byte[] floatBytes){
+		int asInt = (floatBytes[0] & 0xFF) 
+	            | ((floatBytes[1] & 0xFF) << 8) 
+	            | ((floatBytes[2] & 0xFF) << 16) 
+	            | ((floatBytes[3] & 0xFF) << 24);
+		return Float.intBitsToFloat(asInt);
+		
 	}
 
 }
