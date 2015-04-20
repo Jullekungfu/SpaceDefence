dfcf19933350f4639b7b60e125473d459662e51f
Dolck <tim.dolck@gmail.com>
2015-04-20 10:29:48 +0200

    start game from main menu

--- slick-game/src/slimpleslickgame/Game.java
+++ slick-game/src/slimpleslickgame/Game.java
@@ -2,32 +2,63 @@ package slimpleslickgame;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Game extends BasicGameState {
 	public static final int ID = 2;
+	
+	private Player player;
 
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		// TODO: Setup game stuff
 
+		player = new Player();
+		player.init();
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+	public void render(GameContainer gc, StateBasedGame arg1, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-
+		//TODO:render all stuff here
+		player.render(g);
 	}
 
 	@Override
-	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+	public void update(GameContainer gc, StateBasedGame arg1, int delta)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		// TODO: Update all logic
+		processInput(gc.getInput());
+		player.update(delta);
+	}
+	
+	private void processInput(Input input) {
+		Vector2f direction = new Vector2f(0, 0);
 
+		if (input.isKeyDown(Input.KEY_LEFT)) {
+			direction.add(new Vector2f(-1, 0));
+		}
+
+		if (input.isKeyDown(Input.KEY_RIGHT)) {
+			direction.add(new Vector2f(1, 0));
+		}
+
+		if (input.isKeyDown(Input.KEY_UP)) {
+			direction.add(new Vector2f(0, -1));
+		}
+
+		if (input.isKeyDown(Input.KEY_DOWN)) {
+			direction.add(new Vector2f(0, 1));
+		}
+
+		// TODO: add shooting capabilities
+
+		player.setDirection(direction);
 	}
 
 	@Override
