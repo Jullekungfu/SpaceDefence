827ded5bfad92b47eac4f925f82bf6cb71f24995
antonlin1 <anton.lin1@gmail.com>
2015-04-20 11:44:22 +0200

    renamed ClientInput to InThread

---
+++ slick-game/src/client/InThread.java
@@ -0,0 +1,40 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+/**
+ * Reads server input and forwards it to ByteMonitor
+ * @author antonlin
+ *
+ */
+public class InThread extends Thread {
+	private Socket connection;
+	private ByteMonitor monitor;
+
+	public InThread(Socket connection, ByteMonitor monitor) {
+		this.connection = connection;
+		this.monitor = monitor;
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			InputStream input = connection.getInputStream();
+			String line = "";
+			while (!connection.isClosed()) {
+				int c = input.read();
+				line += (char)c;
+				if((char) c == '\n'|| (char) c == '\r'){
+					monitor.putArrayFromServer(line.getBytes());
+					line = "";
+				}
+			}
+			input.close();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
