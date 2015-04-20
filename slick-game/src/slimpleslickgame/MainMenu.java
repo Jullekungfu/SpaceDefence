1d615dfaf159b9934aa73b6dcd3cd267f4b911f4
Jullekungfu <julian.krone@hotmail.com>
2015-04-20 10:04:14 +0200

    add main menu controls

--- slick-game/src/slimpleslickgame/MainMenu.java
+++ slick-game/src/slimpleslickgame/MainMenu.java
@@ -1,40 +1,61 @@
 package slimpleslickgame;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JOptionPane;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
 import client.Client;
 
 public class MainMenu extends BasicGameState {
 	public static final int ID = 1;
 	private StateBasedGame game;
+	private UnicodeFont ufont;
 
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		this.game = game;
-
+		Font font = new Font(Font.MONOSPACED, Font.PLAIN, 60);
+		ufont = new UnicodeFont(font);
+		ufont.addAsciiGlyphs();
+		ufont.addGlyphs(32, 127);
+		ufont.getEffects().add(new ColorEffect(Color.WHITE));
+		ufont.loadGlyphs();
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		g.drawString("NEW GAME", Client.WIDTH/2 - 50, 200);
-		g.drawString("JOIN GAME", Client.WIDTH/2 - 50, 300);
+		g.setFont(ufont);
+		
+		g.drawString("1. NEW GAME", Client.WIDTH/2 - ufont.getWidth("1. NEW GAME")/2, 200);
+		g.drawString("2. JOIN GAME", Client.WIDTH/2 - ufont.getWidth("2. JOIN GAME")/2, 500);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
         Input input = container.getInput();
-		// TODO Auto-generated method stub
-
+        if(input.isKeyDown(Input.KEY_1)){
+        	JOptionPane.showInputDialog("");
+        } else if(input.isKeyDown(Input.KEY_2)){
+        	String ipport = JOptionPane.showInputDialog("Connect to server using ip:port");
+        	game.enterState(Game.ID, new FadeOutTransition(org.newdawn.slick.Color.black), new FadeInTransition(org.newdawn.slick.Color.black));
+    		
+        }
 	}
 
 	@Override
 	public int getID() {
 		return ID;
 	}
-
 }
--- slick-game/src/slimpleslickgame/SplashScreen.java
+++ slick-game/src/slimpleslickgame/SplashScreen.java
@@ -1,12 +1,9 @@
 package slimpleslickgame;
 
-import java.awt.Font;
-
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
@@ -17,19 +14,10 @@ import client.Client;
 public class SplashScreen extends BasicGameState {
 
 	public static final int ID = 0;
-	private StateBasedGame game;
 	private int time;
-	private Font font;
-	private UnicodeFont ufont;
-	private String text;
-	private int tw, th;
-
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-		this.game = game;
-		time = 0;
-
-				
+		time = 0;				
 	}
 
 	@Override
