52e54f069343a78b2c29cc56d0839423e9ec15c6
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-04-20 11:21:15 +0200

    Added socket to outthread.

--- slick-game/src/client/OutThread.java
+++ slick-game/src/client/OutThread.java
@@ -1,15 +1,28 @@
 package client;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.net.Socket;
 
 public class OutThread extends Thread {
 	private OutputStream os;
+	private Socket socket;
 	
-	public OutThread(OutputStream os){
-		this.os = os;
+	public OutThread(Socket s){
+		this.socket = s;
+		try {
+			os = socket.getOutputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void run(){
 		//TODO: Implement.
+		while(!this.socket.isClosed()){
+			
+			
+			
+		}
 	}
 }
