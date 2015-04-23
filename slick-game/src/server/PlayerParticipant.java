8d79fb7096b8e86cb3ae58b69b9311bf36d5b301
Dolck <tim.dolck@gmail.com>
2015-04-23 11:11:14 +0200

    bufferedreader is tha shit

--- slick-game/src/server/PlayerParticipant.java
+++ slick-game/src/server/PlayerParticipant.java
@@ -1,19 +1,20 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class PlayerParticipant extends Thread {
-	private InputStream input;
+	private BufferedReader br;
 	private Socket socket;
 	private Statebox stateBox;
 
 	public PlayerParticipant(Socket socket, Statebox stateBox) {
 		try {
-			this.input = socket.getInputStream();
 			this.socket = socket;
 			this.stateBox = stateBox;
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -23,12 +24,8 @@ public class PlayerParticipant extends Thread {
 	public void run() {
 		try {
 			String message = "";
-			int character;
-			while ((character = input.read()) != -1) {
-				message += character;
-				if ((char) character == '\n' || (char) character == '\r') {
-					stateBox.writeMessage(message.getBytes());
-				}
+			while ((message = br.readLine()) != null) {
+				stateBox.writeMessage(message.getBytes());
 			}
 			socket.shutdownInput();
 			socket.shutdownOutput();
