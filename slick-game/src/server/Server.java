e5bf899755ea7a8d601fd17c8718b4a506d3d41e
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2015-04-23 16:13:59 +0200

    Removed system.out.print

--- slick-game/src/server/Server.java
+++ slick-game/src/server/Server.java
@@ -44,11 +44,8 @@ class UpdateToClient extends Thread {
 	public void addSocket(Socket s) {
 		clients.add(s);
 		byte[] idMessage = new byte[2];
-		//TODO: Create a ID message for the participant.
 		idMessage[0] = EventProtocol.PLAYER_ID;
 		idMessage[1] = (byte) clients.size();
-		System.out.println("Idmessage-flag: " + idMessage[0]);
-		System.out.println("Id: " + idMessage[1]);
 		try {
 			sendMessage(idMessage, s.getOutputStream());
 		} catch (IOException e) {e.printStackTrace();}
