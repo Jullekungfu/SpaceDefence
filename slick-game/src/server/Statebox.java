4c5fa824b99a3ba96703d2c9aa005cdca221ea03
Jullekungfu <julian.krone@hotmail.com>
2015-04-27 09:37:28 +0200

    move clientlist to monitor

--- slick-game/src/server/Statebox.java
+++ slick-game/src/server/Statebox.java
@@ -1,12 +1,21 @@
 package server;
 
+import java.net.Socket;
+import java.util.Vector;
+
 //TODO: This should contain a better buffer. Maybe a ringbuffer? Maybe one buffer per client?
 
 public class Statebox{
     private byte[] message;
+	private Vector<Socket> clients;
 
     public Statebox(){
         this.message = null;
+		this.clients = new Vector<Socket>();
+    }
+    
+    public synchronized int getCurrentClients(){
+    	return clients.size();
     }
 
     public synchronized byte[] readMessage(){
@@ -31,4 +40,16 @@ public class Statebox{
         this.message = msg;
         notifyAll();
     }
+
+	public synchronized void addClient(Socket s) {
+		clients.add(s);
+	}
+	
+	public synchronized Socket[] getClientSockets(){
+		return (Socket[]) clients.toArray();
+	}
+	
+	public synchronized boolean removeClientSocket(Socket s){
+		return clients.remove(s);
+	}
 }
