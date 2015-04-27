1d498000b77675a6adf77df0fe523b6ea1ff5703
antonlin1 <anton.lin1@gmail.com>
2015-04-27 07:41:05 +0200

    added message length in protocol

--- slick-game/src/util/EventProtocol.java
+++ slick-game/src/util/EventProtocol.java
@@ -15,10 +15,13 @@ public abstract class EventProtocol {
 	public static final byte PLAYER_UPGRADED = 0x5;
 	public static final byte PLAYER_ID = 0x6;
 	public static final byte OPPONENT_PLAYER_INIT = 0x7;
-	
+
 	public static final byte CREEP_HP = 0x10;
 	public static final byte CREEP_DIED = 0x11;
 	public static final byte CREEP_SENT = 0x12;
 	public static final byte CREEP_INIT = 0x13;
-	
+
+	// Must be set in last function before being sent to server/client meaning
+	// "putArrayToServer" client side and "sendMessage" server side.
+	public static final byte MESSAGE_LENGTH = 0x20;
 }
