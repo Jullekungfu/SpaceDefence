66ca47c9cfb6d2a0f7ad651369811514943360e9
Dolck <tim.dolck@gmail.com>
2015-04-27 12:01:37 +0200

    moved bullet to center

--- slick-game/src/slimpleslickgame/Bullet.java
+++ slick-game/src/slimpleslickgame/Bullet.java
@@ -5,12 +5,11 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.ShapeFill;
 import org.newdawn.slick.fills.GradientFill;
 import org.newdawn.slick.geom.Circle;
-import org.newdawn.slick.geom.Shape;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Bullet {
 
-	private Shape shape;
+	private Circle shape;
 	private ShapeFill fill;
 	private Vector2f position;
 	private Vector2f dir;
@@ -19,12 +18,13 @@ public class Bullet {
 		this.position = initPos.copy();
 		this.dir = new Vector2f(0, -2);
 		this.shape = new Circle(this.position.x, this.position.y, 5);
-		this.fill = new GradientFill(0, 0, Color.white, 10, 10, Color.white, true);
+		this.fill = new GradientFill(-10, -10, Color.white, 10, 10, Color.white, true);
 	}
 	
 	public void update(int delta){
 		this.position.add(dir);
-		shape.setLocation(position);
+		shape.setCenterX(position.x);
+		shape.setCenterY(position.y);
 	}
 	
 	public void render(Graphics graphics){
