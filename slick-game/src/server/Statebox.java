077265cb567022f769a2a8cc7f7c5e8ee09b0be6
Dolck <tim.dolck@gmail.com>
2015-05-03 14:30:44 +0200

    server fix queue

--- slick-game/src/server/Statebox.java
+++ slick-game/src/server/Statebox.java
@@ -1,16 +1,18 @@
 package server;
 
 import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
 
 //TODO: This should contain a better buffer. Maybe a ringbuffer? Maybe one buffer per client?
 
 public class Statebox{
-    private byte[] message;
+    private Queue<byte[]> messages;
 	private Vector<Socket> clients;
 
     public Statebox(){
-        this.message = null;
+        this.messages = new LinkedList<byte[]>();
 		this.clients = new Vector<Socket>();
     }
     
@@ -19,25 +21,19 @@ public class Statebox{
     }
 
     public synchronized byte[] readMessage(){
-    	while(message == null){ 
+    	while(messages.isEmpty()){ 
             try {
                 wait();
             } catch (InterruptedException e){e.printStackTrace();}
         }
     	
-        byte[] temp = this.message;
-        this.message = null;;
+        byte[] temp = this.messages.poll();
         notifyAll();
         return temp;
     }
 
     public synchronized void writeMessage(byte[] msg){
-       while(message != null){ 
-            try {
-                wait();
-            } catch (InterruptedException e){e.printStackTrace();}
-        }
-        this.message = msg;
+        messages.offer(msg);
         notifyAll();
     }
 
