85c4333a2864960b4ff3a8ab5e5209791c7021ee
Jullekungfu <julian.krone@hotmail.com>
2015-05-11 09:17:12 +0200

    add more logging

--- slick-game/src/client/ParseBytes.java
+++ slick-game/src/client/ParseBytes.java
@@ -110,6 +110,7 @@ public class ParseBytes extends Thread {
 					int creepId = bytesToInt(byteQueue);
 					event = new GameEvent(GameRole.CREEP, creepId);
 					event.setDead();
+					Logger.log("received creep died msg");
 					break;
 				}
 				case EventProtocol.BULLET_INIT: {
