ee127f468a4470ca8073aaa74e04b9306c9eec39
Dolck <tim.dolck@gmail.com>
2015-05-18 09:46:05 +0200

    score should be fixed

--- slick-game/src/slimpleslickgame/OpponentPlayer.java
+++ slick-game/src/slimpleslickgame/OpponentPlayer.java
@@ -44,7 +44,7 @@ public class OpponentPlayer extends Player {
 					if (e.getDirection() != null) {
 						// super.direction = e.getDirection();
 					}
-					if (e.getScore() != -1) {
+					if (e.getScore() > 0) {
 						score = e.getScore();
 					}
 					super.updatePosition(containerShape);
